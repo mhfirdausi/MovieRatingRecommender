e62efe49bbde181fa5624ed319183da1f235bccb
Mohammad Firdausi <mfirda1@tigers.lsu.edu>
2015-11-17 17:46:18 -0600

    Add initial checks for empty/nonexistent tables

--- MovieRatingRecommender.py
+++ MovieRatingRecommender.py
@@ -70,30 +70,32 @@ class Movie(Base):
                                                                                                      self.videoreleasedate,
                                                                                                      self.imdburl)
 
-class Genre():
+
+class Genre(Base):
     __tablename__ = 'genres'
     movieid = Column(Integer, ForeignKey('movies.id'), primary_key=True )
     genre = Column(String(20))
 
-    movie = relationship("Movie", back_populates="genres")
+    #movie = relationship("Movie", back_populates="genres")
 
     def __repr__(self):
         return "<MovieGenre(id ='%i' genre='%s')>" % (self.movieid, self.genre)
 
-# run this only once to create tables
-# TODO: Create seperate function
-Base.metadata.create_all(engine)
 
 from sqlalchemy.orm import sessionmaker
 Session = sessionmaker(bind=engine)
 session = Session()
 
+
+# TODO: Error Checking
+
 users = []
 movies = []
-#TODO: Error Checking
-def userRead(fileName):
-    usersFile = open('data/' + fileName, 'r')
-    for line in usersFile:
+
+
+def userread(filename):
+    usersfile = open(filename, 'r')
+    for line in usersfile:
         user = line.strip('\n').split('|')
         newuser = User(id=int(user[0]), age=int(user[1]), gender=user[2], occupation=user[3], zipcode=user[4])
         users.append(newuser)
@@ -101,22 +103,44 @@ def userRead(fileName):
     session.add_all(users)
     session.commit()
 
-def movieRead(filename):
-    movieFile = open('data/' + filename, 'r')
-    for line in movieFile:
+
+def movieread(filename):
+    moviefile = open(filename, 'r')
+    for line in moviefile:
         movie = line.strip('\n').split('|')
-        if (movie[0] == "521"):
-            continue
         newmovie = Movie(id=int(movie[0]), title=movie[1], releasedate=movie[2], videoreleasedate=movie[3], imdburl=movie[4])
         movies.append(newmovie)
-        print(newmovie)
+        #print(newmovie)
 
     session.add_all(movies)
     session.commit()
 
-# userRead("u.user")
-movieRead("u.item")
-#for u in session.query(User).all():
-#    print(u)
+
+# Main Program execution
+if not(engine.has_table('users')) or not(engine.has_table('movies')) or not(engine.has_table('genres')) or not(engine.has_table('ratings')):
+    print('need to add tables')
+    # run this only once to create tables
+    # TODO: Create seperate function
+    Base.metadata.create_all(engine)
+
+if session.query(User).count() == 0:
+    print("User table empty. Reading in user data...")
+    try:
+        userread("data/u.user")
+        print("User information read in")
+    except IOError as e:
+        print("I/O error({0}): {1}".format(e.errno, e.strerror))
+
+
+if session.query(Movie).count() == 0:
+    print("Movie table empty. Reading in movie data...")
+    try:
+        movieread("data/u.item")
+        print("Movie item information read in")
+    except IOError as e:
+        print("I/O error({0}): {1}".format(e.errno, e.strerror))
+
+for u in session.query(User).all():
+    print(u)
 for m in session.query(Movie).all():
     print(m)
